d43e09a7bbedb0e909f84a7c97fda3444ff388e4
badgalmario <mario.gatica69@gmail.com>
2025-06-09 20:02:32 -0400

    incorporar_resta
    Incorporar función resta

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+v1 = int(input("INDIQUE VALOR "))
+v2 = int(input("INDIQUE VALOR "))
+
+print("El valor de la suma",v1 + v2)
+
+v1 = int(input("INDIQUE VALOR "))
+v2 = int(input("INDIQUE VALOR "))
+
+print("El valor de la resta",v1 - v2)
